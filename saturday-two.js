f1f0f0692049f8d359c3f7f029ed8fc819d1a819
Megan Martel <meganmartel@Megans-MacBook-Air.local>
2022-01-30 18:42:04 -0500

    function practice

--- saturday-two.js
+++ saturday-two.js
@@ -59,14 +59,67 @@
 
 //add all the numbers in the array
 
-let arr_1 = [3, 5, 22, 5, 7, 2, 45, 75, 89, 21, 2]; //276
+// let arr_1 = [3, 5, 22, 5, 7, 2, 45, 75, 89, 21, 2]; //276
 
-function sumOfNumbers (array1) {
-    let sum = 0
-    for (let i = 0; i < array1.length; i++) {
-    sum = sum + array1[i]
-    }
-    return sum
-}
+// function sumOfNumbers (array1) {
+//     let sum = 0
+//     for (let i = 0; i < array1.length; i++) {
+//     sum = sum + array1[i]
+//     }
+//     return sum
+// }
 
-console.log(sumOfNumbers(arr_1))
+// console.log(sumOfNumbers(arr_1))
+
+
+//Given a string change the every second letter to an uppercase ‘Z’. Assume here are no space.
+
+// let str1 = "javascript"; //want output to be jZvZsZrZpZ// keep in mind in javascript a string is like an array of letters so start @ index 0
+
+
+// function changeLetter (string) {
+//     let newString = ""
+//     for (let i = 0; i < string.length; i++) {
+//         if ((i+1) % 2 === 0) {
+//             newString += 'Z' 
+//         } else {
+//             newString += string[i] //newString = newString + string[i]
+//     }
+// }
+//     return newString
+// }
+// //console.log (changeLetter(str1))
+// const result = changeLetter(str1) //can do above or these two steps to be able to use the result variable later 
+// console.log(result)
+
+
+//Write a function that takes age in years and returns age in days 
+
+// let ageYears = 5
+
+// function convertAge (newAge) {
+//     return (newAge * 365)
+// }
+// console.log(convertAge(ageYears))
+
+// write a function that adds two numbers together 
+
+// let numOne = 5
+// let numTwo = 8
+
+// function sumNumbers (num1, num2) {
+//     return (num1 + num2)
+// }
+// console.log(sumNumbers(numOne, numTwo))
+
+// Create a function that takes an array containing only numbers and returns the first element.
+
+// let array = [1, 2, 3, 4, 5, 6];
+
+// function firstElement (arr) {
+//     return arr[0]
+// }
+// console.log(firstElement(array))
+
+
+
